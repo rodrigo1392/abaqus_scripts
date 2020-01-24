fb81104135925f40f2d0517e4aa3b24a29a5221f
rodrigo_1392 <rodrigo.m.rivero13@gmail.com>
2020-01-24 13:42:07 -0300

    Beginning working with R Doe packages

--- first_script.py
+++ first_script.py
@@ -1,3 +1,5 @@
-#print(files_with_extension_lister(r'C:\abaqus_temp\abaqus_scripts', ['py']))
+print(files_with_extension_lister(r'C:\abaqus_temp\abaqus_scripts', ['py']))
 a = mesh_extract_set_nodes(odbs_retrieve_odb_name(0), 'CROWN_OUT')
 print(a, type(a))
+
+print(a)
